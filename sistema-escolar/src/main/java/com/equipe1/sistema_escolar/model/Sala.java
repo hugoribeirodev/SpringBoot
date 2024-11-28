3787fb76a5d53d3960db65f002f43a1317db79ba
Hugo Ribeiro de Sousa <hugorsousa44@gmail.com>
2024-11-28 12:21:51 -0300

    sistema escolar

---
+++ sistema-escolar/src/main/java/com/equipe1/sistema_escolar/model/Sala.java
@@ -0,0 +1,43 @@
+package com.equipe1.sistema_escolar.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "tb_sala")
+public class Sala {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column (name = "name_sala")
+        private String nome;
+
+        @Column (name = "capacidade")
+        private int capacidade;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
